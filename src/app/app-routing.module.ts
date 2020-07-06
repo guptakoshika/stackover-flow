5a8eb5f0a50cfec93473857768037619810443cf
koshika <koshika.gupta@bounceshare.com>
2020-07-06 22:28:41 +0530

    fixed layout for ask question button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
 
 
 const routes: Routes = [
-  // {path: '' , component: LoginComponent},
-  // {path: 'login' , component: LoginComponent },
+  {path: '' , component: LoginComponent},
+  {path: 'login' , component: LoginComponent },
 ];
 
 @NgModule({
